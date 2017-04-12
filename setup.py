e2316f5bd14f91782e8c223302237ad262d7db70
David Downes <me@theukdave.com>
2017-04-12 20:27:23 +0100

    Move to 0.9.2 and update compatible python versions

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-ip-restriction',
-    version='0.9.1',
+    version='0.9.2',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
@@ -29,9 +29,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
