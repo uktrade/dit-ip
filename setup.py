5c00d9be651b7a945f5bca16b51016a2a8644e5b
David Downes <me@theukdave.com>
2017-08-29 10:44:54 +0100

    Release 1.0.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-ip-restriction',
-    version='0.9.4',
+    version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
